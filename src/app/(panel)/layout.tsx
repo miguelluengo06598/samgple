53b157f69f7bb01f7be4bfdbdc13859229a4e07f
Miguelluengo06 <personalthemike@gmail.com>
2026-04-06 00:39:27 +0200

    feat: nueva pagina de contacto

--- src/app/(panel)/layout.tsx
+++ src/app/(panel)/layout.tsx
@@ -1,14 +1,12 @@
-   import NavBottom from '@/components/nav-bottom'
+import NavBottom from '@/components/ui/nav-bottom'
 
-   export default function RootLayout({ children }) {
-     return (
-       <html>
-         <body style={{ display: 'flex' }}>
-           <NavBottom />
-           <main style={{ flex: 1, minWidth: 0 }}>
-             {children}
-           </main>
-         </body>
-       </html>
-      )
-    }
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div style={{ display: 'flex' }}>
+      <NavBottom />
+      <main style={{ flex: 1, minWidth: 0 }}>
+        {children}
+      </main>
+    </div>
+  )
+}
